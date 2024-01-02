1db6fcb1f6543ae9da8e1bc4f8c7355f227bc4ee
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-01-02 14:14:00 -0300

    feat: Adição de estilos
    adicionados os estilos e definida a estrutura da aplicação

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,7 +1,22 @@
 import './CurrencyConverter.css'
 import React from 'react'
 const CurrencyConverter = () => {
-  return <div>CurrencyConverter</div>
+  return (
+    <div className='converter'>
+      <h2>Conversor de Moedas</h2>
+      <input type='number' placeholder='Digite o valor...' />
+      <span>Selecione as Moedas</span>
+      <select>
+        <option value='BRL'>BRL</option>
+      </select>
+      <span>para</span>
+      <select>
+        <option value='USD'>USD</option>
+      </select>
+      <h3>BRL USD</h3>
+      <p>20 BRL valem 5 USD</p>
+    </div>
+  )
 }
 
 export default CurrencyConverter
